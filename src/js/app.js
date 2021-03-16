458733c3d9e11383873d81aaf4e232e840e2e8b6
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-03-16 20:41:00 +0100

    Add booking tab

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,57 @@
 import {settings, select, classNames} from '/js/settings.js';
 import Product from '/js/components/Product.js';
 import Cart from '/js/components/Cart.js';
+import Booking from './Components/Booking.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for(let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageID){
+    const thisApp = this;
+    for(let page of thisApp.pages){
+      if(page.id == pageID){
+        page.classList.add(classNames.pages.active);
+      } else {
+        page.classList.remove(classNames.pages.active);
+      }
+    }
+    for(let link of thisApp.navLinks){
+      if(link.getAttribute('href') == '#' + pageID){
+        link.classList.add(classNames.nav.active);
+      } else {
+        link.classList.remove(classNames.nav.active);
+      }
+    }
+  },
   initData: function(){
     const thisApp = this;
     thisApp.data = {};
@@ -26,6 +75,7 @@ const app = {
   },
   init: function(){
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
   },
   initCart: function(){
@@ -37,8 +87,14 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function(event){
       app.cart.add(event.detail.product.prepareCartProduct());
     });
-  }
+  },
+  initBooking: function(){
+    const thisApp = this;
+    const bookingElem = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingElem);
+  },
 };
 
 app.init();
 app.initCart();
+app.initBooking();
